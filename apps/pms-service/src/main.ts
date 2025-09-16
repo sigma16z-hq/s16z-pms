271f776637b0e21c7aabfc95cf48a6d931c43456
ian <ian@sigma16z.com>
2025-09-16 12:39:09 +0000

    rename to pms-service

--- apps/pms-service/src/main.ts
+++ apps/pms-service/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
 
-  console.log(`S16Z PMS API is running on: http://localhost:${port}`);
+  console.log(`S16Z PMS Service is running on: http://localhost:${port}`);
 }
 
 bootstrap();
